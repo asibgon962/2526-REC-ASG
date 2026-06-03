80a28a2d63426073880cb8d5b9cfdcad761bd865
asibgon962 <asibgon962@fp.iesromerovargas.com>
2026-06-03 14:10:07 +0200

    1.5

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ def read_root():
 
 @app.get("/status")
 def status():
-    return {"message": "FastAPI Sibon-Gonzalez-Alvaro_1.4"}
+    return {"message": "FastAPI Sibon-Gonzalez-Alvaro_1.5"}
 
 
 @app.get("/db-check")
